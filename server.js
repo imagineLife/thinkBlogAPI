fe18994079c1307ba75fb8b65f55506ceae666f7
Your Name <you@example.com>
2017-07-30 08:58:12 -0400

    completed assignment

--- server.js
+++ server.js
@@ -12,19 +12,14 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to BlogPosts
-// so there's some data to look at
-BlogPosts.create('beans', 2);
-BlogPosts.create('tomatoes', 3);
-BlogPosts.create('peppers', 4);
 
 // adding some blogpost content
 // to retrieve.
-BlogPosts.create('1', 'Title1', 'content1', 'author1');
-BlogPosts.create('2', 'Title2', 'content2', 'author2');
-BlogPosts.create('3', 'Title3', 'content3', 'author3');
-BlogPosts.create('4', 'Title4', 'content4', 'author4');
-BlogPosts.create('5', 'Title5', 'content5', 'author5');
+BlogPosts.create('Title1', 'content1', 'author1');
+BlogPosts.create('Title2', 'content2', 'author2');
+BlogPosts.create('Title3', 'content3', 'author3');
+BlogPosts.create('Title4', 'content4', 'author4');
+BlogPosts.create('Title5', 'content5', 'author5');
 
 // when the root of this router is called with GET, return
 // all current BlogPosts items
@@ -33,8 +28,8 @@ app.get('/blog-posts', (req, res) => {
 });
 
 app.post('/blog-posts', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'budget'];
+  // ensure `id`, `title`, `content` & `author` are in the post
+  const requiredFields = [`title`, `content`, `author`];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -44,7 +39,7 @@ app.post('/blog-posts', jsonParser, (req, res) => {
     }
   }
 
-  const item = BlogPosts.create(req.body.name, req.body.budget);
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
   res.status(201).json(item);
 });
 
@@ -53,40 +48,14 @@ app.post('/blog-posts', jsonParser, (req, res) => {
 // item id in updated item object match. if problems with any
 // of that, log error and send back status code 400. otherwise
 // call `BlogPosts.update` with updated item.
-app.put('/shopping-list/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'budget', 'id'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
 
-  if (req.params.id !== req.body.id) {
-    console.log('here');
-    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).send(message);
-  }
-
-  console.log(`Updating shopping list item \`${req.params.id}\``);
-  BlogPosts.update({
-    id: req.params.id,
-    name: req.body.name,
-    budget: req.body.budget
-  });
-  res.status(204).end();
-});
-
-app.put('/blog-posts/:id', jsonParser, (rep,res) => {
-  const requiredFields = ['name', 'id', 'ingredients'];
+app.put('/blog-posts/:id', jsonParser, (req, res) => {
+  const requiredFields = [`title`, `content`, `author`];
   //loop through inputs, if one is missing alert user it is missing
-  for (i=0; i,requiredFields.length; i++) {
+  for (i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const displayMessage = `Dope! You're missing the \`${field}\` field!`;
+      const displayMessage = `Missed the \`${field}\` field!`;
       console.log(displayMessage);
       return res.status(400).send(displayMessage);
     }
@@ -101,11 +70,12 @@ app.put('/blog-posts/:id', jsonParser, (rep,res) => {
   }
 
   //update the recipe with given ID
-  console.log(`Updating recipe \`${req.params.id}\``);
-  Recipes.update({
-    id: req.params.id,
-    name: req.params.name,
-    ingredients: req.params.ingredients
+  console.log(`Updating blog-post \`${req.params.id}\``);
+  BlogPosts.update({
+    id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
   });
   res.status(204).end();
 });
@@ -114,7 +84,7 @@ app.put('/blog-posts/:id', jsonParser, (rep,res) => {
 // try to delete that item from BlogPosts.
 app.delete('/blog-posts/:id', (req, res) => {
   BlogPosts.delete(req.params.id);
-  console.log(`Deleted blog post \`${req.params.ID}\``);
+  console.log(`Deleted blog post \`${req.params.id}\``);
   res.status(204).end();
 });
 
